bdd806cabb17dd7a3dcf2a643973f89016ce1506
SouvikH2002 <hunterdevil494@gmail.com>
2024-08-04 13:30:17 +0530

    added authentication souvik

--- src/app/editor/page.jsx
+++ src/app/editor/page.jsx
@@ -1,105 +1,115 @@
-'use client'
-import { Participant } from './components/Participant'
-import '../css/editor.css'
-import '../css/colors.css'
-import Editor from '@monaco-editor/react'
-import { useState, useEffect, useRef } from 'react'
-import axios from 'axios'
-
+"use client";
+import { Participant } from "./components/Participant";
+import "../css/editor.css";
+import "../css/colors.css";
+import Editor from "@monaco-editor/react";
+import { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import { useUser } from "@clerk/nextjs";
 function editor() {
-  const editorRef = useRef()
-  const [value, setValue] = useState('')
-  const [language, setLanguage] = useState('javascript')
-  const [output, setOutput] = useState('Click run to see the result')
-  const [outputToggle, setOutputToggle] = useState(true)
-  const [codeboxToggle, setCodeboxToggle] = useState(true)
-  const [gptToggle, setGptToggle] = useState(false)
+   const { isLoaded, isSignedIn} = useUser();
+  if (!isLoaded || !isSignedIn) {
+    return null;
+  }
+
+  const editorRef = useRef();
+  const [value, setValue] = useState("");
+  const [language, setLanguage] = useState("javascript");
+  const [output, setOutput] = useState("Click run to see the result");
+  const [outputToggle, setOutputToggle] = useState(true);
+  const [codeboxToggle, setCodeboxToggle] = useState(true);
+  const [gptToggle, setGptToggle] = useState(false);
   const handleOutputToggle = () => {
     if (outputToggle) {
-      document.querySelector('.container .editor .codebox').style.height = '88%'
-      document.querySelector('.container .editor .output').style.height = '10%'
+      document.querySelector(".container .editor .codebox").style.height =
+        "88%";
+      document.querySelector(".container .editor .output").style.height = "10%";
     } else {
-      document.querySelector('.container .editor .codebox').style.height = '70%'
-      document.querySelector('.container .editor .output').style.height = '28%'
+      document.querySelector(".container .editor .codebox").style.height =
+        "70%";
+      document.querySelector(".container .editor .output").style.height = "28%";
     }
-    setOutputToggle(!outputToggle)
-  }
+    setOutputToggle(!outputToggle);
+  };
   const handleGptToggle = () => {
     if (!gptToggle) {
-      document.querySelector('.container .editor').style.width =
-        'calc(45% - 80px)'
+      document.querySelector(".container .editor").style.width =
+        "calc(45% - 80px)";
     } else {
-      document.querySelector('.container .editor').style.width =
-        'calc(76% - 80px)'
-      document.querySelector('.container .participants').style.width = '20%'
+      document.querySelector(".container .editor").style.width =
+        "calc(76% - 80px)";
+      document.querySelector(".container .participants").style.width = "20%";
     }
-    setGptToggle(!gptToggle)
-  }
+    setGptToggle(!gptToggle);
+  };
   const handleCodeboxToggle = () => {
     if (codeboxToggle) {
-      document.querySelector('.container .editor').style.width =
-        'calc(87% - 80px)'
-      document.querySelector('.container .participants').style.width = '10%'
+      document.querySelector(".container .editor").style.width =
+        "calc(87% - 80px)";
+      document.querySelector(".container .participants").style.width = "10%";
     } else {
-      document.querySelector('.container .editor').style.width =
-        'calc(76% - 80px)'
-      document.querySelector('.container .participants').style.width = '20%'
+      document.querySelector(".container .editor").style.width =
+        "calc(76% - 80px)";
+      document.querySelector(".container .participants").style.width = "20%";
     }
-    setCodeboxToggle(!codeboxToggle)
-  }
+    setCodeboxToggle(!codeboxToggle);
+  };
 
   const onMount = (editor) => {
-    editorRef.current = editor
-    editor.focus()
-  }
+    editorRef.current = editor;
+    editor.focus();
+  };
 
   const onSelect = (language) => {
-    setLanguage(language)
-    setValue(CODE_SNIPPETS[language])
-  }
+    setLanguage(language);
+    setValue(CODE_SNIPPETS[language]);
+  };
   const handleCompileRun = async () => {
     const obj = {
-      language: 'java',
-      version: '15.0.2',
+      language: "java",
+      version: "15.0.2",
       files: [
         {
-          name: 'my_cool_code.js',
+          name: "my_cool_code.js",
           content: value,
         },
       ],
-      stdin: '',
-      args: ['1', '2', '3'],
+      stdin: "",
+      args: ["1", "2", "3"],
       compile_timeout: 10000,
       run_timeout: 3000,
       compile_memory_limit: -1,
       run_memory_limit: -1,
-    }
-    const resp = await axios.post('https://emkc.org/api/v2/piston/execute', obj)
-    console.log('checkintg')
-    console.log(resp.data.run.stdout)
-    const outputCode = resp.data.run.stdout.replace(/\n/g, '<br>')
+    };
+    const resp = await axios.post(
+      "https://emkc.org/api/v2/piston/execute",
+      obj
+    );
+    console.log("checkintg");
+    console.log(resp.data.run.stdout);
+    const outputCode = resp.data.run.stdout.replace(/\n/g, "<br>");
 
-    document.querySelector('.showOutput').innerHTML = outputCode
-    setOutput(outputCode)
-  }
+    document.querySelector(".showOutput").innerHTML = outputCode;
+    setOutput(outputCode);
+  };
   return (
-    <div className='container'>
-      <div className='sideBar element'>
-        <div className='logo'></div>
-        <div className='options'>
+    <div className="container">
+      <div className="sideBar element">
+        <div className="logo"></div>
+        <div className="options">
           <div
-            className='option button toggleGPT'
+            className="option button toggleGPT"
             onClick={handleGptToggle}
           ></div>
-          <div className='option button'></div>
-          <div className='option button'></div>
+          <div className="option button"></div>
+          <div className="option button"></div>
         </div>
-        <div className='settings button'></div>
+        <div className="settings button"></div>
       </div>
       {gptToggle ? (
         <>
-          <div className='gpt element'>
-            <div className='cardHeading'>
+          <div className="gpt element">
+            <div className="cardHeading">
               <span>ChatGPT</span>
             </div>
           </div>
@@ -107,23 +117,23 @@ function editor() {
       ) : (
         <></>
       )}
-      <div className='editor element'>
-        <div className='codebox'>
-          <div className='cardHeading'>
+      <div className="editor element">
+        <div className="codebox">
+          <div className="cardHeading">
             <span>Code Editor</span>
-            <div className='options'>
-              <div className='button option'>
+            <div className="options">
+              <div className="button option">
                 <span>languages</span>
               </div>
               <div
-                className='button option expand'
+                className="button option expand"
                 onClick={handleCodeboxToggle}
                 style={
                   gptToggle
                     ? {
                         opacity: 0.5,
-                        cursor: 'not-allowed',
-                        pointerEvents: 'none',
+                        cursor: "not-allowed",
+                        pointerEvents: "none",
                       }
                     : {}
                 }
@@ -133,35 +143,35 @@ function editor() {
             </div>
           </div>
           <Editor
-            height='90vh'
-            theme='vs-dark'
-            defaultLanguage='java'
-            defaultValue='// some comment'
+            height="90vh"
+            theme="vs-dark"
+            defaultLanguage="java"
+            defaultValue="// some comment"
             onMount={onMount}
             value={value}
             onChange={(value) => setValue(value)}
           />
-          <button className='runButton button' onClick={handleCompileRun}>
+          <button className="runButton button" onClick={handleCompileRun}>
             Run code
           </button>
         </div>
-        <div className='output'>
-          <div className='cardHeading'>
+        <div className="output">
+          <div className="cardHeading">
             <span>Output</span>
-            <div className='button toggleOutput' onClick={handleOutputToggle}>
+            <div className="button toggleOutput" onClick={handleOutputToggle}>
               <span>down</span>
             </div>
           </div>
-          <div className='showOutput'></div>
+          <div className="showOutput"></div>
         </div>
       </div>
-      <div className='participants element'>
+      <div className="participants element">
         <Participant></Participant>
         <Participant></Participant>
         <Participant></Participant>
       </div>
     </div>
-  )
+  );
 }
 
-export default editor
+export default editor;
