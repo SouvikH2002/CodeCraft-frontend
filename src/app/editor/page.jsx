6f550564bd4c40eb7fa4e4e16b96b74c5819ea79
SouvikH2002 <hunterdevil494@gmail.com>
2024-08-04 19:43:18 +0530

    Optimize code editor component and add language options

--- src/app/editor/page.jsx
+++ src/app/editor/page.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use client'
 import { Participant } from './components/Participant'
 import '../css/editor.css'
@@ -35,41 +34,6 @@ function editor() {
     setOutputToggle(!outputToggle)
   }
 
-=======
-"use client";
-import { Participant } from "./components/Participant";
-import "../css/editor.css";
-import "../css/colors.css";
-import Editor from "@monaco-editor/react";
-import { useState, useEffect, useRef } from "react";
-import axios from "axios";
-import { useUser } from "@clerk/nextjs";
-function editor() {
-   const { isLoaded, isSignedIn} = useUser();
-  if (!isLoaded || !isSignedIn) {
-    return null;
-  }
-
-  const editorRef = useRef();
-  const [value, setValue] = useState("");
-  const [language, setLanguage] = useState("javascript");
-  const [output, setOutput] = useState("Click run to see the result");
-  const [outputToggle, setOutputToggle] = useState(true);
-  const [codeboxToggle, setCodeboxToggle] = useState(true);
-  const [gptToggle, setGptToggle] = useState(false);
-  const handleOutputToggle = () => {
-    if (outputToggle) {
-      document.querySelector(".container .editor .codebox").style.height =
-        "88%";
-      document.querySelector(".container .editor .output").style.height = "10%";
-    } else {
-      document.querySelector(".container .editor .codebox").style.height =
-        "70%";
-      document.querySelector(".container .editor .output").style.height = "28%";
-    }
-    setOutputToggle(!outputToggle);
-  };
->>>>>>> auth
   const handleGptToggle = () => {
     if (!gptToggle) {
       document.querySelector(".container .editor").style.width =
@@ -79,14 +43,9 @@ function editor() {
         "calc(76% - 80px)";
       document.querySelector(".container .participants").style.width = "20%";
     }
-<<<<<<< HEAD
     setGptToggle(!gptToggle)
   }
 
-=======
-    setGptToggle(!gptToggle);
-  };
->>>>>>> auth
   const handleCodeboxToggle = () => {
     if (codeboxToggle) {
       document.querySelector(".container .editor").style.width =
@@ -105,23 +64,12 @@ function editor() {
     editor.focus();
   };
 
-<<<<<<< HEAD
 
 
   const handleCompileRun = async () => {
     const obj = {
       language: language.language,
       version: language.version,
-=======
-  const onSelect = (language) => {
-    setLanguage(language);
-    setValue(CODE_SNIPPETS[language]);
-  };
-  const handleCompileRun = async () => {
-    const obj = {
-      language: "java",
-      version: "15.0.2",
->>>>>>> auth
       files: [
         {
           name: "my_cool_code.js",
@@ -134,7 +82,6 @@ function editor() {
       run_timeout: 3000,
       compile_memory_limit: -1,
       run_memory_limit: -1,
-<<<<<<< HEAD
     }
     console.log(obj)
     const resp = await axios.post('https://emkc.org/api/v2/piston/execute', obj)
@@ -148,20 +95,6 @@ function editor() {
   useEffect(()=>{
     setValue(language.default)
   },[language])
-=======
-    };
-    const resp = await axios.post(
-      "https://emkc.org/api/v2/piston/execute",
-      obj
-    );
-    console.log("checkintg");
-    console.log(resp.data.run.stdout);
-    const outputCode = resp.data.run.stdout.replace(/\n/g, "<br>");
-
-    document.querySelector(".showOutput").innerHTML = outputCode;
-    setOutput(outputCode);
-  };
->>>>>>> auth
   return (
     <div className="container">
       <div className="sideBar element">
@@ -177,7 +110,6 @@ function editor() {
         <div className="settings button"></div>
       </div>
       {gptToggle ? (
-<<<<<<< HEAD
         <div className='gpt element'>
           <div className='cardHeading'>
             <span>ChatGPT</span>
@@ -205,25 +137,6 @@ function editor() {
             <div className='options'>
               <div className='button option languages'>
                 <Languages setLanguage={setLanguage}></Languages>
-=======
-        <>
-          <div className="gpt element">
-            <div className="cardHeading">
-              <span>ChatGPT</span>
-            </div>
-          </div>
-        </>
-      ) : (
-        <></>
-      )}
-      <div className="editor element">
-        <div className="codebox">
-          <div className="cardHeading">
-            <span>Code Editor</span>
-            <div className="options">
-              <div className="button option">
-                <span>languages</span>
->>>>>>> auth
               </div>
               <div
                 className="button option expand"
@@ -243,16 +156,9 @@ function editor() {
             </div>
           </div>
           <Editor
-<<<<<<< HEAD
             height='90vh'
             theme='vs-dark'
             language={language.language}
-=======
-            height="90vh"
-            theme="vs-dark"
-            defaultLanguage="java"
-            defaultValue="// some comment"
->>>>>>> auth
             onMount={onMount}
             value={value}
             onChange={(value) => setValue(value)}
@@ -261,19 +167,11 @@ function editor() {
             Run code
           </button>
         </div>
-<<<<<<< HEAD
         <div className='output' ref={outputRef}>
           <div className='cardHeading'>
             <span>Output</span>
             <div className='button toggleOutput' onClick={handleOutputToggle}>
               <span>down &#8595;</span>
-=======
-        <div className="output">
-          <div className="cardHeading">
-            <span>Output</span>
-            <div className="button toggleOutput" onClick={handleOutputToggle}>
-              <span>down</span>
->>>>>>> auth
             </div>
           </div>
           <div className="showOutput"></div>
@@ -288,8 +186,4 @@ function editor() {
   );
 }
 
-<<<<<<< HEAD
 export default editor
-=======
-export default editor;
->>>>>>> auth
